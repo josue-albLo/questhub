c5428a468ebfd4cc7f08d6f6f7d535795ca7469a
Josue Guevara <guevarajosue637@gmail.com>
2023-12-03 10:44:51 -0600

    add(contact, about), add(restriccion de votos), fix(rutas de navegacion)

--- questhub/quest/urls.py
+++ questhub/quest/urls.py
@@ -7,11 +7,13 @@ app_name = 'quest'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
-    path("main/", views.mian_page, name='main'),
+    path("main/<int:id>/<str:name>", views.mian_page, name='main'),
     path('createquestion/<int:pk>', views.create_question, name='createquestion'),
     path('processingquestion/<int:pk>', views.procesing_question, name='processingquestion'),
     path('answer/<int:id_question>/<int:pk_user>', views.answer, name='answer'),
     path('createanswer/<int:pk_question>/<int:pk_user>', views.create_answer, name='createanswer'),
     path('save_answer/<int:pk_question>/<int:pk_user>', views.save_answer, name='saveanswer'),
     path('votequestion/<int:pk_question>/<int:pk_user>', views.vote_question, name='votequestion'),
+    path('about/<int:user_id>', views.about, name='about'),
+    path('contact/<int:user_id>', views.contact, name='contact'),
 ]
--- questhub/quest/views.py
+++ questhub/quest/views.py
@@ -16,17 +16,13 @@ def index(request):
 
 
 def login_view(request):
-    print('login view')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        print('Validando credenciales')
         try:
             user = User.objects.get(username=username, password=password)
             if user:
-                print('Credenciales correctas')
-                url = reverse(
-                    'quest:main')+f'?user_id={user.person.id}&user_name={user.person.name}'
+                url = reverse('quest:main', args=[user.person.id, user.person.name])
                 return redirect(url)
         except User.DoesNotExist:
             messages.error(
@@ -38,29 +34,25 @@ def login_view(request):
 
 
 @require_http_methods(['GET'])
-def mian_page(request):
-    print(request.user)
-    user_id = request.GET['user_id']
-    user_name = request.GET['user_name']
-    print(f'Informacion del usuario {user_id} {user_name}')
+def mian_page(request, id, name):
     questions = Question.objects.annotate(
         count_answers=Count('answer', distinct=True), 
         count_votes=Count('votequestion__id', distinct=True))[::-1][:10]
-    context = {'questions': questions, 'user': user_name, 'id': user_id}
-    print(questions)
+    context = {'questions': questions, 'user': name, 'id': id}
     return render(request, 'quest/main.html', context)
 
 
 @require_http_methods(['GET'])
 def create_question(request, pk):
     category = QuestionCategory.objects.all()
-    context = {'categories': category, 'user_id': pk}
+    user = User.objects.get(pk=pk)
+    context = {'categories': category, 'id': pk, 'name':user.person.name}
     return render(request, 'quest/create_question.html', context)
 
 
 @require_http_methods(['POST'])
 def procesing_question(request, pk):
-    user = Person.objects.get(pk=pk)
+    user = User.objects.get(pk=pk)
     
     post_data = request.POST.copy()
     post_data['author'] = pk
@@ -69,28 +61,27 @@ def procesing_question(request, pk):
     if form_question.is_valid():
         form_question.save()
         messages.success(request, 'Pregunta creada exitosamente')
-        url = reverse('quest:main') + \
-            f'?user_id={user.id}&user_name={user.name}'
+        url = reverse('quest:main', args=[user.id, user.person.name])
         return redirect(url)
     else:
-        print('Formulario invalido')
-        url = reverse('quest:main') + \
-            f'?user_id={user.id}&user_name={user.name}'
+        url = reverse('quest:main', args=[user.id, user.person.name])
         return redirect(url)
     
 def answer(request, id_question, pk_user):
     question = Question.objects.get(pk=id_question)
+    person = User.objects.get(pk=pk_user)
     answers_question = Answer.objects.filter(question=question)
-    context = {'question': question, 'answers': answers_question, 'user_id': pk_user}
+    context = {'question': question, 'answers': answers_question, 'id': pk_user, 'name': person.person.name}
     return render(request, 'quest/answers.html', context)
 
 def create_answer(request, pk_question, pk_user):
     question = Question.objects.get(pk=pk_question)
-    context = {'question': question, 'user_id':pk_user}
+    user = User.objects.get(pk=pk_user)
+    context = {'question': question, 'id':pk_user,'name':user.person.name}
     return render(request, 'quest/create_answers.html', context)
 
 def save_answer(request, pk_question, pk_user):
-    user = Person.objects.get(pk=pk_user)
+    user = User.objects.get(pk=pk_user)
     post_data = request.POST.copy()
     post_data['question'] = pk_question
     post_data['author'] = pk_user
@@ -99,10 +90,10 @@ def save_answer(request, pk_question, pk_user):
     if (form_answer.is_valid()):
         form_answer.save()
         messages.success(request, 'Respuesta creada exitosamente')
-        return redirect(reverse('quest:main')+f'?user_id={user.id}&user_name={user.name}')
+        return redirect(reverse('quest:main', args=[user.id, user.person.name]))
     else:
         messages.error(request, 'Error al crear la respuesta')
-        return redirect(reverse('quest:main')+f'?user_id={user.id}&user_name={user.name}')
+        return redirect(reverse('quest:main', args=[user.id, user.person.name]))
     
 def vote_question(request, pk_question, pk_user):
     post_data = request.POST.copy()
@@ -110,10 +101,24 @@ def vote_question(request, pk_question, pk_user):
     post_data['question']=pk_question
     post_data['author']=pk_user
     post_data['vote']="upvote"
+    
+    verify_vote = VoteQuestion.objects.filter(question=pk_question, author=pk_user)
+    
     form_vote = VoteQuestionForm(post_data)
-    print(request.GET)
-    if(form_vote.is_valid()):
+    if(form_vote.is_valid() and not verify_vote):
         form_vote.save()
         messages.success(request, 'Voto exitoso')
-        return redirect(reverse('quest:main')+f'?user_id={pk_user}&user_name={user.person.name}')
-    return redirect(reverse('quest:main')+f'?user_id={pk_user}&user_name={user.person.name}')
+        return redirect(reverse('quest:main', args=[user.person.id, user.person.name]))
+    else:
+        messages.error(request, 'Error al votar, ya has votado')
+        return redirect(reverse('quest:main', args=[user.person.id, user.person.name]))
+
+def about(request, user_id):
+    user = User.objects.get(pk=user_id)
+    context = {'id': user_id, 'name': user.person.name}
+    return render(request, 'quest/about.html', context )
+
+def contact(request, user_id):
+    user = User.objects.get(pk=user_id)
+    context = {'id': user_id, 'name': user.person.name}
+    return render(request, 'quest/contact.html', context)
